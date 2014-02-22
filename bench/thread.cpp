899f5c1e6ec2171661b23e819d95152db3e97928
Romain Failliot <romain.failliot@foolstep.com>
2014-02-21 19:38:47 -0500

    Add copyrights and GPL license in source files.

--- bench/thread.cpp
+++ bench/thread.cpp
@@ -1,8 +1,20 @@
 /*
- * thread.cpp
+ * Copyright (C) 2014 Romain "Creak" Failliot.
  *
- *  Created on: 2013-09-25
- *      Author: creak
+ * This file is part of libbench.
+ *
+ * libbench is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libbench is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libbench. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <assert.h>
--- tool/libbenchwindow.cpp
+++ tool/libbenchwindow.cpp
@@ -1,8 +1,20 @@
 /*
- * libbenchwindow.cpp
+ * Copyright (C) 2014 Romain "Creak" Failliot.
  *
- *  Created on: 2013-10-13
- *      Author: creak
+ * This file is part of libbench.
+ *
+ * libbench is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libbench is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libbench. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <iostream>
